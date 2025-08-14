bd9e4c84c27fc239221069edb05a60fa58382ac3
onegkgbran <onegkg@brandeis.edu>
2025-08-13 20:09:19 -0500

    simple refactoring

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
 import datetime
 import json
+import sys
 
 import requests
 
 
 def main():
     location = None
+    default_zmanim = None
     with open(".config") as f:
-        content = f.read()
-        lines = content.split("\n")
-        for line in lines:
-            print(line)
-            if line.startswith("LOCATION_1"):
-                location = line.split("=", maxsplit=1)[1].strip()
-            # if line.startswith("LOCATION_2"):
-            #     if loc_index == "-2":
-            #     location2 = line.split("=", maxsplit=1)[1].strip()
-
+        location, new_dict = read_config(f, default_zmanim)
     if location is None:
-        raise Exception("no location provided")
+        location = input("enter the geonames.org identifier for your desired location")
+
     date = datetime.date.today().strftime("%Y-%m-%d")
+
+    data = api_call(location, date)
+
+    print(json.dumps(data, indent=4))
+
+
+def read_config(f, dictionary):
+    location = None
+    if dictionary is not None:
+        new_dict = dictionary.copy()
+    else:
+        new_dict = None
+    try:
+        content = f.read()
+    except Exception:
+        print("file problem")
+        sys.exit(1)
+    lines = content.split("\n")
+    for line in lines:
+        if line.startswith("location_1"):
+            location = line.split("=", maxsplit=1)[1].strip()
+    return (location, new_dict)
+
+
+def api_call(location, date):
     api_url = f"https://www.hebcal.com/zmanim?cfg=json&geonameid={location}&date={date}"
     params = {"cfg": "json"}
-
     response = requests.get(api_url, params=params)
+
     print(response)
     if response.status_code == 200:
         data = response.json()
         print(data)
+        return data
     else:
         raise Exception("Error:", response.status_code, response.text)
 
-    print(json.dumps(data, indent=4))
+
+def test_json():
+    with open("data.json", "r", encoding="utf-8") as file:
+        return json.load(file)
 
 
 if __name__ == "__main__":
