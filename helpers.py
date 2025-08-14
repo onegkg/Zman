6a03b530d3e28e5a6dd8d91dc4327fae658b023f
onegkgbran <onegkg@brandeis.edu>
2025-08-13 20:40:33 -0500

    moved helper functions to helpers.py and nicely print the raw data, also added support for customizing which data is supported through config file

---
+++ helpers.py
@@ -0,0 +1,72 @@
+import json
+import sys
+
+import requests
+
+
+def read_config(f, dictionary):
+    location = None
+    new_dict = dictionary.copy()
+    try:
+        content = f.read()
+    except Exception:
+        print("file problem")
+        sys.exit(1)
+    lines = content.split("\n")
+    for line in lines:
+        if line.startswith("location_1"):
+            location = line.split("=", maxsplit=1)[1].strip()
+    return (location, new_dict)
+
+
+def api_call(location, date):
+    api_url = f"https://www.hebcal.com/zmanim?cfg=json&geonameid={location}&date={date}"
+    params = {"cfg": "json"}
+    response = requests.get(api_url, params=params)
+
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data
+    else:
+        raise Exception("Error:", response.status_code, response.text)
+
+
+def test_json():
+    with open("example.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def make_default_dict():
+    default = {
+        "chatzotNight": False,
+        "alotHaShachar": False,
+        "misheyakir": False,
+        "misheyakirMachmir": False,
+        "dawn": False,
+        "sunrise": True,
+        "sofZmanShmaMGA19Point8": False,
+        "sofZmanShmaMGA16Point1": False,
+        "sofZmanShmaMGA": False,
+        "sofZmanShma": True,
+        "sofZmanTfillaMGA19Point8": False,
+        "sofZmanTfillaMGA16Point1": False,
+        "sofZmanTfillaMGA": False,
+        "sofZmanTfilla": True,
+        "chatzot": True,
+        "minchaGedola": True,
+        "minchaGedolaMGA": False,
+        "minchaKetana": True,
+        "minchaKetanaMGA": False,
+        "plagHaMincha": True,
+        "sunset": True,
+        "beinHaShmashos": False,
+        "dusk": True,
+        "tzeit7083deg": False,
+        "tzeit85deg": False,
+        "tzeit42min": False,
+        "tzeit50min": False,
+        "tzeit72min": False,
+    }
+    return default
--- main.py
+++ main.py
@@ -1,60 +1,29 @@
 import datetime
-import json
-import sys
 
-import requests
+from helpers import make_default_dict, read_config, test_json  # , api_call
 
 
 def main():
     location = None
-    default_zmanim = None
+    default_zmanim = make_default_dict()
     with open(".config") as f:
-        location, new_dict = read_config(f, default_zmanim)
+        location, zmanim_bool = read_config(f, default_zmanim)
     if location is None:
         location = input("enter the geonames.org identifier for your desired location")
 
     date = datetime.date.today().strftime("%Y-%m-%d")
 
-    data = api_call(location, date)
+    # data = api_call(location, date)
+    data = test_json()
 
-    print(json.dumps(data, indent=4))
+    times = data["times"]
 
+    for k, v in times.items():
+        if zmanim_bool[k]:
+            date_object = datetime.datetime.strptime(v[:-6], "%Y-%m-%dT%H:%M:%S")
 
-def read_config(f, dictionary):
-    location = None
-    if dictionary is not None:
-        new_dict = dictionary.copy()
-    else:
-        new_dict = None
-    try:
-        content = f.read()
-    except Exception:
-        print("file problem")
-        sys.exit(1)
-    lines = content.split("\n")
-    for line in lines:
-        if line.startswith("location_1"):
-            location = line.split("=", maxsplit=1)[1].strip()
-    return (location, new_dict)
-
-
-def api_call(location, date):
-    api_url = f"https://www.hebcal.com/zmanim?cfg=json&geonameid={location}&date={date}"
-    params = {"cfg": "json"}
-    response = requests.get(api_url, params=params)
-
-    print(response)
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
-        return data
-    else:
-        raise Exception("Error:", response.status_code, response.text)
-
-
-def test_json():
-    with open("data.json", "r", encoding="utf-8") as file:
-        return json.load(file)
+            do_formatted = date_object.strftime("%I:%M:%S %p")
+            print(f"{k}: {do_formatted}")
 
 
 if __name__ == "__main__":
